5c4a6c38690ef94aaff1ef16f50707530b4e4dee
EomTaeWoo <umtanggg@naver.com>
2022-08-23 23:45:29 +0900

    챕터1

---
+++ BackCoding/src/BasicMath01/Chap01.java
@@ -0,0 +1,23 @@
+package BasicMath01;
+
+import java.util.Scanner;
+
+public class Chap01 {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.nextInt();	//불변 비용
+		int b = sc.nextInt();	//가변 비용
+		int c = sc.nextInt();	//상품 가격
+		
+		if (c <= b) {
+			System.out.println("-1");
+		}else {
+			System.out.println(a/(c-b)+1);
+		}
+		
+	}
+
+}
