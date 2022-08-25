3708ba91fbf6dc904d115895c75132ad8eb0bf97
EomTaeWoo <umtanggg@naver.com>
2022-08-25 16:20:16 +0900

    거스름돈 문제

--- BackCoding/src/ThisCodingTest/Grid/Chap01.java
+++ BackCoding/src/ThisCodingTest/Grid/Chap01.java
@@ -1,7 +1,21 @@
 package ThisCodingTest.Grid;
 
+import java.util.Scanner;
+
 public class Chap01 {
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
+		int money = sc.nextInt();
+		int N = 0;
+		
+		int[] coin = {500,100,50,10};
+		
+		for (int i = 0; i < coin.length; i ++) {
+			N += money / coin[i];
+			money = money % coin[i];
+		}
+		
+		System.out.println(N);
 	}
 }
