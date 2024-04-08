425d9b5b94494cc7421d4df697b95a9008a455a8
EomTaeWoo <umtanggg@naver.com>
2024-04-08 14:52:10 +0900

    Q01.문자찾기

--- JavaCodingTest/src/Chap01_String/Q01.java
+++ JavaCodingTest/src/Chap01_String/Q01.java
@@ -1,4 +1,7 @@
 package Chap01_String;
+
+import java.util.Scanner;
+
 /**
  * 
  * 문자 찾기
@@ -16,13 +19,33 @@ public class Q01 {
 	
 	public int solution(String str, char t) {
 		int answer = 0;
+		str = str.toUpperCase();
+		t = Character.toUpperCase(t);	// char변수라서 Character함수를 써야함
+		
+		/*
+		for(int i = 0; i < str.length(); i ++) {
+			if(str.charAt(i) == t) {
+				answer ++;
+			}
+		}
+		*/
+		
+		for(char x : str.toCharArray()) {
+			if(x == t) {
+				answer ++;
+			}
+		}
 		
 		return answer;
 	}
 
 	public static void main(String[] args) {
 		Q01 T = new Q01();
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		char t = sc.next().charAt(0);	// 문자열에서 문자한개를 가져온다.
 		
+		System.out.println(T.solution(str, t));
 	}
 
 }
