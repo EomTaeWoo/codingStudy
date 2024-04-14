8aff535e775eb4b4815a165f5a917393eae25757
EomTaeWoo <umtanggg@naver.com>
2024-04-14 09:14:30 +0900

    Chap02_Q01_큰 수 출력하기

---
+++ JavaCodingTest/src/Chap02_Array/Q02.java
@@ -0,0 +1,10 @@
+package Chap02_Array;
+
+public class Q02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ JavaCodingTest/src/Chap02_Array/Qtest.java
@@ -0,0 +1,21 @@
+package Chap02_Array;
+
+import java.util.Scanner;
+
+public class Qtest {
+
+	public String solution(String str) {
+		String answer = "";
+		
+		return answer;
+	}
+
+	public static void main(String[] args) {
+		Q01 T = new Q01();
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		
+		
+	}
+
+}
