e6ccec604220c5c01193aafd84b125ad7144ab53
EomTaeWoo <umtanggg@naver.com>
2024-05-11 09:49:19 +0900

    올바른 괄호_Stack

--- JavaCodingTest/src/Chap05_Stack_Queue/Q01.java
+++ JavaCodingTest/src/Chap05_Stack_Queue/Q01.java
@@ -1,10 +1,37 @@
 package Chap05_Stack_Queue;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class Q01 {
+	
+	public String solution(String str) {
+		String answer = "YES";
+		
+		Stack<Character> stack = new Stack<>();
+		for(char x : str.toCharArray()) {
+			if(x == '(') {
+				stack.push(x);
+			} else  {
+				if(stack.isEmpty()) {
+					return "NO";
+				}
+				stack.pop();
+			}
+		}
+		if(!stack.isEmpty()) {
+			return "NO";
+		}
+		
+		return answer;
+	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Q01 T = new Q01();
+		
+		Scanner sc = new Scanner(System.in);
+		String str = sc.next();
+		System.out.println(T.solution(str));
 	}
 
 }
