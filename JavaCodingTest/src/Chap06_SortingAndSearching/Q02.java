9c14e6772794fac45374a38855cf1601625d1764
EomTaeWoo <umtanggg@naver.com>
2024-06-05 00:24:31 +0900

    버블정렬

---
+++ JavaCodingTest/src/Chap06_SortingAndSearching/Q02.java
@@ -0,0 +1,33 @@
+package Chap06_SortingAndSearching;
+
+import java.util.Scanner;
+
+public class Q02 {
+	
+	public int[] solution(int n, int[] arr) {
+		for(int i = 0; i < n; i ++) {
+			for(int j = 0; j < n - i - 1; j ++) {
+				if(arr[j] > arr[j + 1]) {
+					int tmp = arr[j];
+					arr[j] = arr[j + 1];
+					arr[j + 1] = tmp;
+				}
+			}
+		}
+		return arr;
+	}
+	
+	public static void main(String[] args) {
+		Q01 T = new Q01();
+		Scanner sc = new Scanner(System.in);
+		int n = sc.nextInt();
+		int[] arr = new int[n];
+		for(int i = 0; i < n; i ++) {
+			arr[i] = sc.nextInt();
+		}
+		for(int x : T.solution(n, arr)) {
+			System.out.print(x + " ");
+		}
+	}
+
+}
